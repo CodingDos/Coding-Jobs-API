7805e8eb133f4d725128de3582204e4e5628c268
Steven V <sgvillacis@gmail.com>
2024-03-11 10:51:31 -0400

    Changed db name

--- db/connections.js
+++ db/connections.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 mongoose.set("returnOriginal", false)
 
-const MONGODB_URI = process.env.MONGODB_PROD || 'mongodb://localhost:27017/hogwarts'
+const MONGODB_URI = process.env.MONGODB_PROD || 'mongodb://localhost:27017/CodingJobs'
 
 mongoose.connect(MONGODB_URI)
 
